4e6d528e5ca1aeaf46cec10458867cadaab809ff
Dareos Khalili <khalilidareos@gmail.com>
2020-11-18 08:06:12 +0100

    caches the repos instead of making an api call every time you change tab on the navbar

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -33,7 +33,7 @@ export default class Popular extends React.Component {
 
     this.state = {
       selectedLanguage: 'All',
-      repos: null,
+      repos: {},
       error: null
     }
     this.updateLanguage = this.updateLanguage.bind(this)
@@ -47,26 +47,34 @@ export default class Popular extends React.Component {
   updateLanguage(selectedLanguage){
     this.setState({
       selectedLanguage,
-      repos: null,
       error: null
     })
 
+  if (!this.state.repos[selectedLanguage]) {
     fetchPopularRepos(selectedLanguage)
-    .then((repos) => this.setState({
-      repos,
-      error: null,
-    }))
-    .catch(() => {
-      console.warn('Error fetching repos', error)
-
-      this.setState({
-        error: 'There was an error fetching the repositories.'
+      .then((data) => {
+        console.log(data)
+        this.setState(({ repos }) => ({
+          repos: {
+            ...repos,
+            [selectedLanguage]: data
+          }
+        }))
       })
-    })
+      .catch(() => {
+        console.warn('Error fetching repos: ', error)
+
+        this.setState({
+          error: `There was an error fetching the repositories.`
+        })
+      })
+    }
   }
 
   isLoading(){
-    return this.state.repos === null && this.state.error === null
+    const { selectedLanguage, repos, error } = this.state
+
+    return !repos[selectedLanguage] && error === null
   }
 
   render() {
@@ -83,7 +91,7 @@ export default class Popular extends React.Component {
 
         {error && <p>{error}</p>}
 
-        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+        {repos[selectedLanguage] && <pre>{JSON.stringify(repos[selectedLanguage], null, 2)}</pre>}
       </React.Fragment>
     )
   }
